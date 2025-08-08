65cba5675e5b65f6d2da87d77850b85dfd3fa39c
Shiv <shivkube@gmail.com>
2025-08-08 11:37:48 +0530

    - Work in progress for unit test files

---
+++ api/server_middleware.go
@@ -0,0 +1,57 @@
+package api
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	middleware "github.com/oapi-codegen/nethttp-middleware"
+)
+
+func NewServerWithMiddleware() http.Handler {
+
+	server := NewServer()
+
+	r := http.NewServeMux()
+
+	ctx := context.Background()
+
+	// Middleware section
+	// openapi validation middleware
+	// loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: true}
+	loader := openapi3.NewLoader()
+	spec, _ := loader.LoadFromFile("openapi/api.yaml")
+	// Validate document
+	_ = spec.Validate(ctx)
+	valmw := middleware.OapiRequestValidatorWithOptions(spec,
+		&middleware.Options{
+			DoNotValidateServers: true,
+		})
+
+	// logging middleware
+	logmw := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var (
+				ip     = r.RemoteAddr
+				method = r.Method
+				url    = r.URL.String()
+				proto  = r.Proto
+			)
+
+			userAttrs := slog.Group("user", "ip", ip)
+			requestAttrs := slog.Group("request", "method", method, "url", url, "proto", proto)
+
+			slog.Info("request received", userAttrs, requestAttrs)
+			next.ServeHTTP(w, r)
+		})
+	}
+	// get an `http.Handler` that we can use
+	h := HandlerWithOptions(server,
+		StdHTTPServerOptions{
+			BaseRouter:  r,
+			Middlewares: []MiddlewareFunc{valmw, logmw},
+		})
+
+	return h
+}
---
+++ api/server_middleware_test.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPostSignupPhone(t *testing.T) {
+
+	url := "/signup/phone"
+
+	// add request body
+	number := 9886240527
+	data := PostSignupPhoneJSONBody{Phone: &number}
+
+	// Marshal the struct to JSON
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// add query params
+	q := req.URL.Query()
+	q.Add("type", "driver")
+	req.URL.RawQuery = q.Encode() // Encode and assign the query string
+
+	// Set appropriate headers
+	req.Header.Set("Content-Type", "application/json")
+
+	// recorder
+	rr := httptest.NewRecorder()
+
+	// create the handler.
+	h := NewServerWithMiddleware()
+
+	// invoke
+	h.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+}
